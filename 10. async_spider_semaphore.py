19867c724ae0628756922b333af2e1d8e5ed85b9
cztps2 <cztps2@gmail.com>
2021-01-10 23:48:18 +0800

    asyncio semaphore

---
+++ 10. async_spider_semaphore.py
@@ -0,0 +1,29 @@
+import asyncio
+import aiohttp
+import blog_spider
+
+semaphore = asyncio.Semaphore(10)
+
+
+async def async_craw(url):
+    async with semaphore:
+        print("craw url: ", url)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                result = await resp.text()
+                await asyncio.sleep(5)
+                print(f"craw url: {url}, {len(result)}")
+
+
+loop = asyncio.get_event_loop()
+
+tasks = [
+    loop.create_task(async_craw(url))
+    for url in blog_spider.urls]
+
+import time
+
+start = time.time()
+loop.run_until_complete(asyncio.wait(tasks))
+end = time.time()
+print("use time seconds: ", end - start)
